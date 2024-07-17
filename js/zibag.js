10d27bc27f7d268f2f676241403fc1f7377aff33
jeongwoo <yang12798@naver.com>
2024-07-17 09:37:48 +0900

    first commit

---
+++ js/zibag.js
@@ -0,0 +1,9 @@
+// $(document).ready(function() {
+//     $(window).scroll(function() { // 스크롤 이벤트를 감지
+//         if ($(this).scrollTop() > 100) { // 스크롤이 100px 이상 되었을 때
+//             $('.fix').addClass('sticky'); // sticky 클래스를 추가하여 배경색 변경
+//         } else {
+//             $('.fix').removeClass('sticky'); // 100px 미만이면 sticky 클래스를 제거하여 초기 상태로 돌림
+//         }
+//     });
+// });
